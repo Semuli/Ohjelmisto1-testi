515749a591f941f262f8a9d673b792fe3ad1e449
Mikko Ilvonen <mikkoilvonen94@gmail.com>
2024-08-20 15:13:20 +0300

    Tehtävä 2

---
+++ Tehtävä2/Tehtävä2.3.py
@@ -0,0 +1,8 @@
+Kanta_str = input("Hei anna suorakulmionkulmion kanta:")
+korkeus_str = input("Hei anna suorakulmion korkeus:")
+kanta = float(Kanta_str)
+korkeus = float(korkeus_str)
+Pintaala = kanta * korkeus
+Piiri = kanta + kanta + korkeus + korkeus
+print("Pinta-ala on" + str(Pintaala))
+print("Piiri on" + str(Piiri))

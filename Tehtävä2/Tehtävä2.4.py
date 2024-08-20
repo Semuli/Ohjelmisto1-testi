49079a7d84eb8a05267aa19c59a9e819bb31134f
Mikko Ilvonen <mikkoilvonen94@gmail.com>
2024-08-20 15:43:29 +0300

    Tehtävä 2

---
+++ Tehtävä2/Tehtävä2.4.py
@@ -0,0 +1,10 @@
+print("kerro 3 kokonaislukua:")
+Luku_1 = input()
+Luku_2 = input()
+Luku_3 = input()
+Summa = float(Luku_1) + float(Luku_2) + float(Luku_3)
+Tulo = float(Luku_1) * float(Luku_2) * float(Luku_3)
+Keskiarvo = float(Summa) / 3
+print("Lukujen Summa on:" + str(Summa))
+print("Lukujen Tulo on:" + str(Tulo))
+print("Lukujen Keskiar on:" + str(Keskiarvo))

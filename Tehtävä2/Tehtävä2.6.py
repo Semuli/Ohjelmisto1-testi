abb719f1c4c18fe5540d941936b8b70f036ce941
Mikko Ilvonen <mikkoilvonen94@gmail.com>
2024-08-21 12:02:08 +0300

    Tehtävä 2

---
+++ Tehtävä2/Tehtävä2.6.py
@@ -0,0 +1,13 @@
+import random
+random_number1 = random.randint(0, 9)
+random_number2 = random.randint(0, 9)
+random_number3 = random.randint(0, 9)
+
+random_number4 = random.randint(1, 6)
+random_number5 = random.randint(1, 6)
+random_number6 = random.randint(1, 6)
+random_number7 = random.randint(1, 6)
+
+print("Numerokoodisi ovat:")
+print(random_number1, random_number2, random_number3)
+print(random_number4, random_number5, random_number6, random_number7)

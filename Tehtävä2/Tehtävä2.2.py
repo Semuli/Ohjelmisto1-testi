526b5eb17540816ebab509d05a2da6a0061ed3c7
Mikko Ilvonen <mikkoilvonen94@gmail.com>
2024-08-21 12:07:39 +0300

    Tehtävä 2

--- Tehtävä2/Tehtävä2.2.py
+++ Tehtävä2/Tehtävä2.2.py
@@ -1,5 +1,5 @@
 import math
 Säde_str = input("kerro ympyrän säde")
 säde = float(Säde_str)
-Pintaala = (säde*säde)*math.pi
+Pintaala = math.pi * (säde ** 2)
 print("Ympyrän Pinta-ala on: " + str(Pintaala) + ".")

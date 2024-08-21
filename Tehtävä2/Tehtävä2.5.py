ae586d0dea1859be36903c26cf2df8bc91d6ba06
Mikko Ilvonen <mikkoilvonen94@gmail.com>
2024-08-21 11:39:01 +0300

    Tehtävä 2

--- Tehtävä2/Tehtävä2.5.py
+++ Tehtävä2/Tehtävä2.5.py
@@ -3,16 +3,12 @@ Leiviskä = input("leiviskä: ")
 Naula = input("Naula: ")
 Luoti = input("Luoti: ")
 
-Leiviskä_paino = float(Leiviskä) * 20
-Naula_paino = (Leiviskä_paino + float(Naula)) * 32
-Luoti_paino = (Naula_paino + float(Luoti)) * 13.3
+Luoti_paino = 13.3
+Naula_paino = (Luoti_paino) * 32
+Leiviskä_paino = (Naula_paino) * 20
 
-Massa = (Leiviskä_paino + Naula_paino + Luoti_paino)
+Massa = (Leiviskä_paino * float(Leiviskä)) + (Naula_paino * float(Naula)) + (Luoti_paino * float(Luoti))
 Massa_kilot = int(Massa/1000)
 Massa_grammat = Massa - (Massa_kilot * 1000)
-
-print(Massa_grammat)
-print(f"{Massa_kilot:2.0f}")
-print("massa grammoina:" + str(Massa))
 
 print(f"{Massa_kilot:2.0f} kilogrammaa ja {Massa_grammat:<.2f} grammaa."  )

3a21d433590165a067cc12087f5071287a647e6b
Mikko Ilvonen <mikkoilvonen94@gmail.com>
2024-08-25 13:35:30 +0300

    Tehtävä 2

--- Tehtävä3/Tehtävä3.2.py
+++ Tehtävä3/Tehtävä3.2.py
@@ -1,11 +1,11 @@
 Hytti = input("kerro mikä hytti luokka (LUX, A, B, C):")
-if Hytti == "LUX":
+if Hytti.upper() == "LUX":
     print("LUX on parvekkeellinen hytti yläkannella.")
-elif Hytti == "A":
+elif Hytti.upper() == "A":
     print("A on ikkunallinen hytti autokannen yläpuolella.")
-elif Hytti == "B":
+elif Hytti.upper() == "B":
     print("B on ikkunaton hytti autokannen yläpuolella.")
-elif Hytti == "C":
+elif Hytti.upper() == "C":
     print("C on ikkunaton hytti autokannen alapuolella.")
 else:
     print("Virheellinen hyttiluokka")

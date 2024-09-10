f831f5c5afd75e39f3161413f1cb35b6e1fe8011
Mikko Ilvonen <mikkoilvonen94@gmail.com>
2024-09-10 14:19:14 +0300

    tehtävä8

--- Tehtävä8/Tehtävä8.2.py
+++ Tehtävä8/Tehtävä8.2.py
@@ -2,13 +2,10 @@ import mysql.connector
 
 def get_airport_type_amount(iso_country):
     sql = f'SELECT type, count(*) FROM airport WHERE iso_country = "{iso_country}" GROUP BY type'
-    cursor = connection.cursor()
+    cursor = connection.cursor(dictionary=True)
     cursor.execute(sql)
     airport_data = cursor.fetchall()
     return airport_data
-
-
-
 
 connection = mysql.connector.connect(
     host='localhost',
@@ -24,4 +21,4 @@ iso_country = input("Kerro maatunnus:")
 tyyppi_maarat = get_airport_type_amount(iso_country)
 
 for tyyppi in tyyppi_maarat:
-    print(f"Tyyppi:{tyyppi[0]} määrä:{tyyppi[1]}")
+    print(f"Tyyppi:{tyyppi['type']} määrä:{tyyppi['count(*)']}")
